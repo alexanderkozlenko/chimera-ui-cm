5ec579ea70f818e54f22e33307d1b02fb3aea23e
Alexander Kozlenko <alexander.kozlenko@live.com>
2018-10-11 22:59:06 +0300

    Add benchmarks for bindable command

---
+++ src/Anemonis.UI.ComponentModel.Benchmarks/TestSuites/BindableCommandBenchmarks.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Anemonis.UI.ComponentModel.Benchmarks.TestSuites
+{
+    public sealed class BindableCommandBenchmarks
+    {
+        private readonly BindableCommand<object> _observableCommand0 = new BindableCommand<object>(p => { });
+        private readonly BindableCommand<object> _observableCommand1 = new BindableCommand<object>(p => { });
+        private readonly BindableCommand<object> _observableCommand2 = new BindableCommand<object>(p => { });
+
+        public BindableCommandBenchmarks()
+        {
+            _observableCommand2.CanExecuteChanged += OnCanExecuteChanged;
+        }
+
+        private void OnCanExecuteChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void CommandAction(object parameter)
+        {
+        }
+
+        private bool CommandPredicate(object parameter)
+        {
+            return true;
+        }
+
+        [Benchmark(Description = "CanExecute")]
+        public bool CanExecute1()
+        {
+            return _observableCommand0.CanExecute(null);
+        }
+
+        [Benchmark(Description = "Execute")]
+        public void Execute()
+        {
+            _observableCommand0.Execute(null);
+        }
+
+        [Benchmark(Description = "RaiseCanExecuteChanged-NPC=N")]
+        public void RaisePropertyChanged()
+        {
+            _observableCommand1.RaiseCanExecuteChanged();
+        }
+
+        [Benchmark(Description = "RaiseCanExecuteChanged-NPC=Y")]
+        public void RaisePropertyChangedWithEventSubscriber()
+        {
+            _observableCommand2.RaiseCanExecuteChanged();
+        }
+    }
+}
